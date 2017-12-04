c2b5378d13dbccf0f54d3a2499036993481535f1
Mattia Lamouroux <mattia.lamouroux@outlook.fr>
2017-12-04 14:56:30 +0100

    create query model

---
+++ db/migrate/20171204135536_create_queries.rb
@@ -0,0 +1,14 @@
+class CreateQueries < ActiveRecord::Migration[5.0]
+  def change
+    create_table :queries do |t|
+      t.string :adress
+      t.string :activity
+      t.integer :radius_search
+      t.integer :radius_catchment_area
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
