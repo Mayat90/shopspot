7e5b6596cc7d9bc8d4a2838c9199e4ad4562132b
Tiphanie Tourniaire <tiphanietourniaire@hotmail.com>
2017-12-05 16:30:09 +0100

    form

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,2 +1,7 @@
 class Query < ApplicationRecord
+  validates :address, presence: true
+  validates :activity, presence: true
+  validates :radius_search, presence: true
+  validates :radius_catchment_area, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :queries
-  root to: 'pages#home'
+  root to: 'queries#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
