758979e0be56861decad09308348e4390f19bccb
Christophe GACHET <maildecri@gmail.com>
2017-12-05 14:58:35 +0100

    scrapinsee DONE

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,18 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
   def home
   end
+
+  def insee
+    lat = params["lat"].to_f
+    long = params["long"].to_f
+    zoom = params["zoom"].to_i
+    delta = 0.01
+
+    @tiles= Tiles.find(lat-delta, long-delta, lat + delta, long + delta, zoom)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "/insee/:zoom/:lat/:long", to: "pages#insee" , :constraints => { :lat => /[^\/]+/, :long => /[^\/]+/ }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
