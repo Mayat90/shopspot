d3ebcc2ef6106e50d72ea7b8c6b6e637cc524ab4
Mattia Lamouroux <mattia.lamouroux@outlook.fr>
2017-12-08 14:59:58 +0100

    end en trop :)

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -13,13 +13,12 @@ class CompetitorsController < ApplicationController
     result_search = RestClient.get(url)
     results = JSON.parse(result_search)
     # binding.pry
-        competitor.phone_number = result["result"]["formatted_phone_number"]
-        competitor.address = result["result"]["formatted_address"]
-        competitor.opening_hours = result["result"]["weekday_text"]
-        competitor.rating = result["result"]["rating"]
-        competitor.number_rating = result["result"]["reviews"].count
-        competitor.update
-      end
+      competitor.phone_number = result["result"]["formatted_phone_number"]
+      competitor.address = result["result"]["formatted_address"]
+      competitor.opening_hours = result["result"]["weekday_text"]
+      competitor.rating = result["result"]["rating"]
+      competitor.number_rating = result["result"]["reviews"].count
+      competitor.update
     end
   end
 
