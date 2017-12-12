ee1b686abcf45173f35910591b1e0afff9320aed
Mattia Lamouroux <mattia.lamouroux@outlook.fr>
2017-12-12 18:28:38 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ CSV.foreach('lib/seeds/cities.csv', encoding: 'iso-8859-1:utf-8') do |row|
   area: row[2],
   longitude: row[3],
   latitude: row[4],
-  population: row[5]
+  population: row[5],
+  sexe: {homme: row[6], femme: row[7]},
+  age: {jeune: row[8], adulte: row[9], senior: row[10]},
+  chomage: row[11],
+  revenu: row[12]
    )
 end
 
