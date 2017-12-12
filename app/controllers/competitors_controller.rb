d47da7ea92caf1056eded9917a5f658dae511f60
Mattia Lamouroux <mattia.lamouroux@outlook.fr>
2017-12-12 19:37:21 +0100

    Seed and geolocate for info on show

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -8,19 +8,18 @@ class CompetitorsController < ApplicationController
 
   def find_details(competitors)
     competitors.each do |competitor|
-    url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{competitor.place_id}&key=#{ENV['GOOGLE_API_BROWSER_KEY']}"
-    result_search = RestClient.get(url)
-    results = JSON.parse(result_search)
-      competitor.phone_number = results["result"]["formatted_phone_number"]
-      competitor.address = results["result"]["formatted_address"]
-      competitor.opening_hours = results["result"]["weekday_text"]
-      competitor.rating = results["result"]["rating"]
-      if results["result"]["reviews"].nil? == false
-        competitor.number_rating = results["result"]["reviews"].count
+        url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=#{competitor.place_id}&key=#{ENV['GOOGLE_API_BROWSER_KEY']}"
+        result_search = RestClient.get(url)
+        results = JSON.parse(result_search)
+          competitor.phone_number = results["result"]["formatted_phone_number"]
+          competitor.address = results["result"]["formatted_address"]
+          competitor.opening_hours = results["result"]["weekday_text"]
+          competitor.rating = results["result"]["rating"]
+          if results["result"]["reviews"].nil? == false
+            competitor.number_rating = results["result"]["reviews"].count
+          end
+          competitor.save
       end
-      competitor.save
-
-    end
   end
 
   private
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,3 @@
 class City < ApplicationRecord
-
+  reverse_geocoded_by :latitude, :longitude
 end
